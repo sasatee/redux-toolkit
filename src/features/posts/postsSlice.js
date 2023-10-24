a241c7707f59a3b355fd5cbbc299f48276bd1976
sasatee <123723844+sasatee@users.noreply.github.com>
2023-10-24 14:00:35 +0400

    extraReducers

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,14 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
+import axios from "axios";
+import { sub } from "date-fns";
+
+
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
 const initialState = {
+  posts: [],
+  status: "idle", // idle ,  | loading | succeded | failed
+  error: null,
+};
 
-    posts:[],
-    status:"idle", // idle ,  | loading | succeded | failed
-    error:null,
-}
-  
-
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await axios.get(POSTS_URL);
+  return  response.data;
+});
 
 const postsSlice = createSlice({
   name: "posts",
@@ -46,6 +53,39 @@ const postsSlice = createSlice({
       }
     },
   },
+  extraReducers(builder){
+    builder // swith case statement
+
+    //promise coukd be pending and then we set status to loading
+    .addCase(fetchPosts.pending,(state,action)=>{
+        state.status="loading"
+    })
+    //promise coukd be succeeded and appended some information  and set status loading = succeeded
+    .addCase(fetchPosts.fulfilled,(state,action)=>{
+        state.status="suceeded"
+        //adding date and reactions
+        let min = 1; 
+        const loadedPosts = action.payload.map(post =>{
+            post.date =sub(new Date(),{minutes:min++}).toISOString();
+            post.reactions={
+                thumbsUp: 0,
+                wow: 0,
+                heart: 0,
+                rocket: 0,
+                coffee: 0
+
+            }
+            return post;
+
+        });
+          // add any fetched posts to the array 
+        state.posts = state.posts.concat(loadedPosts)
+    })
+    .addCase(fetchPosts.rejected,(state,action)=>{
+        state.status ="failed"
+        state.error = action.error.message
+    })
+  }
 });
 
 export const selectAllPost = (state) => state.posts.posts;
